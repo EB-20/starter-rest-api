120efd349235ef3d111e7fdc6c16c7367185030f
mayank.kumar@ebix.com <mayank.kumar@ebix.com>
2023-12-06 15:11:02 +0530

    ddf

--- uploadFile/route/logout.route.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const {logout} = require('../controller/logout.controller');
-const { verifyJwtToken } = require('../utils/token.utils');
-
-router.post('/',verifyJwtToken,logout)
-
-module.exports = router;
--- uploadFile/route/seeder.route.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const {getKey} = require('../seeder')
-const {verifyJwtToken} = require('../utils/token.utils');
-const {verifyPermission} = require('../utils/accessVerify');
-
-router.get('/getKey',verifyJwtToken,verifyPermission(['view-KEY']), getKey)
-
-module.exports= router;
